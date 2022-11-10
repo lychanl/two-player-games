31c865d4f87eb53d6b502cdc12a26fa1c1a09962
stapaw <stanislawpawlak3@gmail.com>
2022-11-10 08:47:01 +0100

    refactor: one pick game with size parameter

--- two_player_games/games/Pick.py
+++ two_player_games/games/Pick.py
@@ -6,51 +6,50 @@ from two_player_games.player import Player
 from two_player_games.state import State
 
 
-class Pick15(Game):
-    """Class that represents the Pick15 game"""
+class Pick(Game):
+    """Class that represents the Pick game"""
     FIRST_PLAYER_DEFAULT_CHAR = '1'
     SECOND_PLAYER_DEFAULT_CHAR = '2'
 
-    def __init__(self, first_player: Player = None, second_player: Player = None):
+    def __init__(self, first_player: Player = None, second_player: Player = None, n: int = 4):
         """
         Initializes game.
 
         Parameters:
             first_player: the player that will go first (if None is passed, a player will be created)
             second_player: the player that will go second (if None is passed, a player will be created)
+            n: the subset size of picked numbers that should sum up to aim value
         """
         self.first_player = first_player or Player(self.FIRST_PLAYER_DEFAULT_CHAR)
         self.second_player = second_player or Player(self.SECOND_PLAYER_DEFAULT_CHAR)
 
-        state = Pick15State(self.first_player, self.second_player)
+        state = PickState(self.first_player, self.second_player, n)
 
         super().__init__(state)
 
 
-class Pick15Move(Move):
+class PickMove(Move):
     """
-    Class that represents a move in the Pick15Move game
+    Class that represents a move in the PickMove game
 
     Variables:
-        number: selected number (from 1 to 9)
+        number: selected number (from 1 to n^2)
     """
 
     def __init__(self, number: int):
         self.number = number
 
     def __eq__(self, o: object) -> bool:
-        if not isinstance(o, Pick15Move):
+        if not isinstance(o, PickMove):
             return False
         return self.number == o.number
 
 
-class Pick15State(State):
-    """Class that represents a state in the Pick15State game"""
-    MAX_NUMBER = 9
-    AIM_VALUE = 15
+class PickState(State):
+    """Class that represents a state in the PickState game"""
 
     def __init__(self,
-                 current_player: Player, other_player: Player,
+                 current_player: Player, other_player: Player, n,
                  current_player_numbers: List[int] = None,
                  other_player_numbers: List[int] = None):
         """Creates the state. Do not call directly."""
@@ -63,13 +62,16 @@ class Pick15State(State):
         self.current_player_numbers = current_player_numbers
         self.other_player_numbers = other_player_numbers
         self.selected_numbers = set(self.current_player_numbers).union(self.other_player_numbers)
+        self.n = n
+        self.max_number = n ** 2
+        self.aim_value = int((n ** 2 * (n ** 2 + 1)) / (2 * n))
         super().__init__(current_player, other_player)
 
-    def get_moves(self) -> Iterable[Pick15Move]:
-        return [Pick15Move(number) for number in range(1, self.MAX_NUMBER + 1) if number not in self.selected_numbers]
+    def get_moves(self) -> Iterable[PickMove]:
+        return [PickMove(number) for number in range(1, self.max_number + 1) if number not in self.selected_numbers]
 
-    def make_move(self, move: Pick15Move) -> 'Pick15State':
-        if move.number > self.MAX_NUMBER or move.number in self.selected_numbers:
+    def make_move(self, move: PickMove) -> 'PickState':
+        if move.number > self.max_number or move.number in self.selected_numbers:
             raise ValueError("Invalid move")
         else:
             self.current_player_numbers.append(move.number)
@@ -80,32 +82,33 @@ class Pick15State(State):
             other_player = self._current_player
             other_player_numbers = self.current_player_numbers
 
-        return Pick15State(
-            next_player, other_player, next_player_numbers, other_player_numbers
+        return PickState(
+            next_player, other_player, self.n, next_player_numbers, other_player_numbers
         )
 
     def is_finished(self) -> bool:
-        return self._check_if_sums_to_15(self.current_player_numbers) or \
-               self._check_if_sums_to_15(self.other_player_numbers) or \
-               len(self.selected_numbers) == self.MAX_NUMBER
+        return self._check_if_sums_to_aim_value(self.current_player_numbers) or \
+               self._check_if_sums_to_aim_value(self.other_player_numbers) or \
+               len(self.selected_numbers) == self.max_number
 
     def get_winner(self) -> Optional[Player]:
         if not self.is_finished():
             return None
-        if self._check_if_sums_to_15(self.current_player_numbers):
+        if self._check_if_sums_to_aim_value(self.current_player_numbers):
             return self._current_player
-        elif self._check_if_sums_to_15(self.other_player_numbers):
+        elif self._check_if_sums_to_aim_value(self.other_player_numbers):
             return self._other_player
         else:
             return None
 
     def __str__(self) -> str:
-        return f"Current player: {self._current_player.char}, Numbers: " \
+        return f"n: {self.n}, aim_value: {self.aim_value}" \
+               f"\nCurrent player: {self._current_player.char}, Numbers: " \
                f"{'[]' if not self.current_player_numbers else sorted(self.current_player_numbers)}," \
                f"\nOther player: {self._other_player.char}, Numbers: " \
                f"{'[]' if not self.other_player_numbers else sorted(self.other_player_numbers)}"
 
     # below are helper methods for the public interface
 
-    def _check_if_sums_to_15(self, numbers: List[int]) -> bool:
-        return self.AIM_VALUE in [sum(i) for i in itertools.combinations(numbers, 3)]
+    def _check_if_sums_to_aim_value(self, numbers: List[int]) -> bool:
+        return self.aim_value in [sum(i) for i in itertools.combinations(numbers, self.n)]
